1ff43661fef6fdcda23b43777d96403af2408c2f
Peter Šulek <psulek@gmail.com>
2017-01-23 19:05:59 +0100

    updated typescript def

--- index.d.ts
+++ index.d.ts
@@ -4,8 +4,12 @@ import { Component } from 'react';
 
 export interface WebViewProps extends React.HTMLAttributes<Electron.WebViewElement>, React.ClassAttributes<WebView> {
   src: string
-  
-  autosize?: boolean
+
+  autosize?: string;
+  minwidth?: string;
+  minheight?: string;
+  maxwidth?: string;
+  maxheight?: string;
   disablewebsecurity?: boolean
   httpreferrer?: string
   nodeintegration?: boolean
@@ -18,7 +22,7 @@ export interface WebViewProps extends React.HTMLAttributes<Electron.WebViewEleme
   blinkfeatures?: string
   disableblinkfeatures?: string
   guestinstance?: string
-  
+
   onLoadCommit?: EventListener
   onDidFinishLoad?: EventListener
   onDidFailLoad?: EventListener
@@ -28,21 +32,32 @@ export interface WebViewProps extends React.HTMLAttributes<Electron.WebViewEleme
   onDidGetResponseDetails?: EventListener
   onDidGetRedirectRequest?: EventListener
   onDomReady?: EventListener
-  onPageTitleSet?: EventListener
+  onPageTitleUpdated?: EventListener
   onPageFaviconUpdated?: EventListener
   onEnterHtmlFullScreen?: EventListener
   onLeaveHtmlFullScreen?: EventListener
   onConsoleMessage?: EventListener
+  onFoundInPage?: EventListener
   onNewWindow?: EventListener
+  onWillNavigate?: EventListener
+  onDidNavigate?: EventListener
+  onDidNavigateInPage?: EventListener
+  onDestroyed?: EventListener
+  onMediaStartedPlaying?: EventListener
+  onMediaPaused?: EventListener
+  onDidChangeThemeColor?: EventListener
+  onUpdateTargetUrl?: EventListener
+  onDevtoolsOpened?: EventListener
+  onDevtoolsClosed?: EventListener
+  onDevtoolsFocused?: EventListener
   onClose?: EventListener
   onIpcMessage?: EventListener
   onCrashed?: EventListener
   onGpuCrashed?: EventListener
   onPluginCrashed?: EventListener
-  onDestroyed?: EventListener
 }
 interface WebViewState {
   loaded: boolean
   webview: Electron.WebViewElement
 }
-export default class WebView extends Component<WebViewProps, WebViewState> {}  
+export default class WebView extends Component<WebViewProps, WebViewState> {}
